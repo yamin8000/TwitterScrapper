9cab1eb6da71c6f6a1b02b4e16a74d9fb1948429
YaMiN <yamin8000@yahoo.com>
2023-04-01 04:17:04 +0330

    sanitize html links/ a tags

--- src/main/kotlin/io/github/yamin8000/twitterscrapper/Main.kt
+++ src/main/kotlin/io/github/yamin8000/twitterscrapper/Main.kt
@@ -4,10 +4,24 @@ import io.github.yamin8000.twitterscrapper.helpers.client
 import io.github.yamin8000.twitterscrapper.helpers.httpGet
 import org.jsoup.Jsoup
 import org.jsoup.select.Elements
+import java.util.regex.Pattern
 
 fun main() {
+    println("Enter username to fetch posts:")
+    val username = readlnOrNull()
+    if (username != null) {
+        getUserPosts(username).forEachIndexed { index, item ->
+            println(index)
+            println(item)
+        }
+    }
+}
+
+private fun getUserPosts(
+    username: String,
+    limit: Int = 100
+): List<String> {
     val base = "https://nitter.net/"
-    val profile = "yamin"
     var cursor = ""
 
     val twits = mutableListOf<String>()
@@ -15,7 +29,7 @@ fun main() {
 
     var html: String
     do {
-        html = client.httpGet("$base$profile?cursor=$cursor")
+        html = client.httpGet("$base$username?cursor=$cursor")
         if (html.isNotBlank()) {
             val doc = Jsoup.parse(html)
             hasMoreIndicator = doc.select("div[class^=show-more] a").attr("href")
@@ -23,12 +37,18 @@ fun main() {
             val threads = doc.select("div[class^=thread-line]")
             twits.addAll(getSingles(doc.allElements))
             twits.addAll(getSingles(threads))
-            println("twits  count: ${twits.size}")
+            if (twits.size >= limit)
+                break
         }
     } while (hasMoreIndicator.isNotBlank())
-    twits.forEachIndexed { i, item ->
-        println(i)
-        println(item)
+    return twits.take(limit).map { removeLinks(it) }
+}
+
+fun removeLinks(
+    twit: String
+): String {
+    return Pattern.compile("<a.*</a>").matcher(twit).replaceAll {
+        it.group().substringAfter(">").substringBefore("</a>")
     }
 }
 
