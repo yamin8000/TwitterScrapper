6ba89c7e1a1108adf59cd165b83b2dd64f266792
YaMiN <yamin8000@yahoo.com>
2023-04-10 04:03:18 +0330

    play elon musk song in the background

--- src/main/kotlin/io/github/yamin8000/twitterscrapper/Main.kt
+++ src/main/kotlin/io/github/yamin8000/twitterscrapper/Main.kt
@@ -1,9 +1,28 @@
 package io.github.yamin8000.twitterscrapper
 
+import com.soywiz.korau.sound.playAndWait
+import com.soywiz.korau.sound.readAudioStream
+import com.soywiz.korio.async.launch
+import com.soywiz.korio.file.std.toVfs
 import io.github.yamin8000.twitterscrapper.modules.MainModule
 import io.github.yamin8000.twitterscrapper.modules.settings.Config
+import io.github.yamin8000.twitterscrapper.util.Utility.getResource
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import java.io.File
 
 fun main() {
+    runCatching {
+        CoroutineScope(Dispatchers.IO).launch {
+            elonMusk()
+        }
+    }
     Config()
     MainModule().run()
+}
+
+suspend fun elonMusk() {
+    val elon = File("elonmusk.mp3")
+    if (elon.exists())
+            elon.toVfs().readAudioStream().playAndWait()
 }
